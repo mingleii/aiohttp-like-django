2c5fc65a35cdab19833f5f6c3a077ba26fe33a8e
minglei.weng <minglei.weng@dianjoy.com>
2017-01-19 19:03:41 +0800

    复制代码的注释~

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-#############################################
-# File Name: setup.py
-# Author: xingming
-# Mail: huoxingming@gmail.com
-# Created Time:  2015-12-11 01:25:34 AM
-#############################################
-
-
 from setuptools import setup, find_packages
 
 setup(
